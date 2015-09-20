20f2f2c82e633c598195297bcbf511f0a6125a6c
Brian Kleszyk <brian.kleszyk@gartner.com>
2015-09-20 18:33:18 -0400

    Added license and config

--- setup.py
+++ setup.py
@@ -6,13 +6,16 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'minpages',
-        version = '0.1',
+        version = '0.0.1',
         description = 'An abbreviated man-pages', 
-        long_description = """\
-                Hello!""",
+        long_description = """
+A simple way to make a man page or add a reminder""",
+        url = 'http://github.com/brian-bk/minpages/',
+        license='GPL',
         author = 'Brian Kleszyk',
         author_email = 'bkleszyk@gmail.com',
-        packages = find_packages(),
+        py_modules=['minpages'],
+        packages = find_packages(exclude=['tests/*']),
         data_files=[('minpages/pages',glob('pages/*'))],
         entry_points = {
             'console_scripts': ['min = minpages.min:main']
